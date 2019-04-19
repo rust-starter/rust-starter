2cef587e64947ffb143ff3afb006dc019ba52483
Abid Omar <contact@omarabid.com>
2019-04-19 14:06:54 +0100

    feat(human_panic): Add human_panic library

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,10 +1,15 @@
 #[macro_use]
 pub extern crate failure;
+#[macro_use]
+extern crate human_panic;
 
 pub mod utils;
 
 use utils::error::Result;
 
 pub fn start() -> Result<()> {
+    // Setup human-panic
+    setup_panic!();
+
     Ok(())
 }
