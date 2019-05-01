68e634aecb36e19c8ca4d280fe7e0a02092fb56f
Abid Omar <contact@omarabid.com>
2019-05-01 23:19:52 +0100

    tests(predicate): add a predicates-based test

--- core/tests/test_cli.rs
+++ core/tests/test_cli.rs
@@ -26,3 +26,15 @@ fn test_hazard_exit_code() {
     cmd.arg("hazard").assert().code(0);
 }
 
+#[test]
+fn test_hazard_stdout() {
+    let hazard_predicate = predicate::function(|x: &str| {
+        if (x == "You got it right!\n" || x == "You got it wrong!\n") {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    let mut cmd = Command::cargo_bin("rust-starter").expect("Calling binary failed");
+    cmd.arg("hazard").assert().stdout(hazard_predicate);
+}
