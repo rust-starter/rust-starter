f325d82ce8007f8f23c54ce71f2a11b428f31f71
Abid Omar <contact@omarabid.com>
2020-10-13 02:49:37 +0100

    imp(error): add error simulation code

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,9 +1,10 @@
 use utils::error::Result;
 use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
 
 /// Return, randomly, true or false
 pub fn simulate_error() -> Result<()> {
+    // Trigger an error
+    File::open("thisfiledoesnotexist")?;
+
     Ok(())
 }
