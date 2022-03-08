bb3edda3c012237ab641e44e3073db0233802c62
Abid Omar <contact@omarabid.com>
2022-03-08 12:25:20 +0100

    refactor(config): downgrade Config to 0.11

--- utils/src/app_config.rs
+++ utils/src/app_config.rs
@@ -1,17 +1,15 @@
 use config::{Config, Environment};
-use lazy_static::lazy_static;
+use lazy_static::{__Deref, lazy_static};
 use serde::{Deserialize, Serialize};
-use std::ops::Deref;
 use std::path::Path;
-use std::sync::{Arc, Mutex};
+use std::sync::RwLock;
 
 use super::error::Result;
 
 // CONFIG static variable. It's actually an AppConfig
 // inside an RwLock.
 lazy_static! {
-    pub static ref CONFIG: Arc<Mutex<Config>> =
-        Arc::new(Mutex::new(Config::builder().build().unwrap()));
+    pub static ref CONFIG: RwLock<Config> = RwLock::new(Config::new());
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -28,31 +26,33 @@ pub struct AppConfig {
 impl AppConfig {
     /// Initialize AppConfig.
     pub fn init(default_config: Option<&str>) -> Result<()> {
-        let mut settings = Config::builder().add_source(Environment::with_prefix("APP"));
+        let mut settings = Config::new();
 
         // Embed file into executable
         // This macro will embed the configuration file into the
         // executable. Check include_str! for more info.
         if let Some(config_contents) = default_config {
             //let contents = include_str!(config_file_path);
-            let default_source = config::File::from_str(config_contents, config::FileFormat::Toml);
-            settings = settings.add_source(default_source);
+            settings.merge(config::File::from_str(
+                config_contents,
+                config::FileFormat::Toml,
+            ))?;
         }
 
-        let config_build = settings.build()?;
-
-        // TODO: Merge settings with Clap Settings Arguments
+        // Merge settings with env variables
+        settings.merge(Environment::with_prefix("APP"))?; // TODO: Merge settings with Clap Settings Arguments
 
         // Save Config to RwLoc
-        let mut w = CONFIG.lock()?;
-        *w = config_build;
-        drop(w);
+        {
+            let mut w = CONFIG.write()?;
+            *w = settings;
+        }
 
         Ok(())
     }
 
     pub fn merge_args(app: &mut clap::App) -> Result<()> {
-        dbg!(CONFIG.lock()?.clone().deserialize::<Vec<String>>());
+        //dbg!(CONFIG.lock()?.clone().deserialize::<Vec<String>>());
 
         Ok(())
     }
@@ -62,7 +62,7 @@ impl AppConfig {
         if let Some(config_file_path) = config_file {
             {
                 CONFIG
-                    .lock()?
+                    .write()?
                     .merge(config::File::with_name(config_file_path.to_str().unwrap()))?;
             }
         }
@@ -71,10 +71,10 @@ impl AppConfig {
 
     // Set CONFIG
     pub fn set(key: &str, value: &str) -> Result<()> {
-        // Set Property
-        let mut guard = CONFIG.lock()?;
-        guard.set(key, value)?;
-        drop(guard);
+        {
+            // Set Property
+            CONFIG.write()?.set(key, value)?;
+        }
 
         Ok(())
     }
@@ -84,10 +84,7 @@ impl AppConfig {
     where
         T: serde::Deserialize<'de>,
     {
-        let guard = CONFIG.lock()?;
-        let value = guard.get::<T>(key)?;
-        drop(guard);
-        Ok(value)
+        Ok(CONFIG.read()?.get::<T>(key)?)
     }
 
     // Get CONFIG
@@ -95,14 +92,12 @@ impl AppConfig {
     // This means you have to fetch this again if you changed the configuration.
     pub fn fetch() -> Result<AppConfig> {
         // Get a Read Lock from RwLock
-        let r = CONFIG.lock()?;
+        let r = CONFIG.read()?;
 
         // Clone the Config object
         let config_clone = r.deref().clone();
 
-        drop(r);
-
         // Coerce Config into AppConfig
-        Ok(config_clone.try_deserialize().unwrap())
+        Ok(config_clone.try_into()?)
     }
 }
